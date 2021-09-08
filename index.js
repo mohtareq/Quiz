a5d583a347ab3c724710115edf3ae6155f5ebbf3
mohtareq <mtareq571@gmail.com>
2021-09-08 21:43:31 +0300

    first page done

---
+++ index.js
@@ -0,0 +1,22 @@
+// Js
+let quotes = [
+"Are you over 18 to enter our competition?"
+];
+
+let color = [
+    "yellow"
+];
+
+let removedcolor = [
+    "#bbb"
+];
+
+function newQuote() {
+    let randomNumber = Math.floor(Math.random() * quotes.length);
+    let randomColor = Math.floor(Math.random() * quotes.length);
+    let randomRemovedcolor = Math.floor(Math.random() * quotes.length);
+    document.getElementById(`text`).innerHTML = quotes[randomNumber];
+    document.getElementById(`dot3`).style.background = color[randomColor];
+    document.getElementById(`dot1`).style.background = removedcolor[randomRemovedcolor];
+}
+
