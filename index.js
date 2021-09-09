fcceddcd47970c7fbc01ab205b65330610abb0f1
mohtareq <mtareq571@gmail.com>
2021-09-09 21:41:52 +0300

    two pages

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// Js
-let quotes = [
-"Are you over 18 to enter our competition?"
-];
-
-let color = [
-    "yellow"
-];
-
-let removedcolor = [
-    "white"
-];
-
-
-
-function newQuote() {
-    let randomNumber = Math.floor(Math.random() * quotes.length);
-    let randomColor = Math.floor(Math.random() * quotes.length);
-    let randomRemovedcolor = Math.floor(Math.random() * quotes.length);
-    document.getElementById(`text`).innerHTML = quotes[randomNumber];
-    document.getElementById(`dot3`).style.background = color[randomColor];
-    document.getElementById(`dot1`).style.background = removedcolor[randomRemovedcolor];
+const newQuote = () => {
+    document.getElementById(`text`).innerHTML = "Are you over 18 to enter our competition?";
+    document.getElementById(`dot3`).style.background = "yellow";
+    document.getElementById(`dot1`).style.background = "white";
 }
+
+var timesClicked = 0;
+
+$("#yesButton").click(function() {
+    timesClicked++;
+
+    if (timesClicked > 1) {
+        return window.location.href = "http://127.0.0.1:5500/boxes/index.html";
+    }
+})
+
+$("#noButton").click(function() {
+    timesClicked++;
+
+    if (timesClicked > 1) {
+        return window.location.href = "http://127.0.0.1:5500/boxes/index.html";
+    }
+})
