8d6b6153a1b685143492259c75fe4e17717a1068
mohtareq <mtareq571@gmail.com>
2021-09-10 16:03:47 +0300

    responsive

--- BoxesPage/boxes.js
+++ BoxesPage/boxes.js
@@ -2,3 +2,45 @@ function PopUp(){
     document.getElementById('popup-container').style.display="none"; 
     document.querySelectorAll('.scrollbar-hidden')[0].style.overflow = "auto"
 }
+
+/*
+@media (max-width: 320px) {
+  .header-flex {
+    padding: 10px;
+  }
+  
+  .header-logo {
+    width: 100px;
+  }
+  .popup-div {
+    width: 280px;
+    margin: 10px 5px;
+  }
+}
+
+@media (max-width: 375px) {
+  .header-flex {
+    padding: 10px;
+  }
+  
+  .header-logo {
+    width: 100px;
+  }
+
+  .popup-div {
+    width: 320px;
+    margin: 10px 10px;
+  }
+}
+
+@media (max-width: 425px) {
+  .header-flex {
+    padding: 15px;
+  }
+  
+  .header-logo {
+    width: 130px;
+  }
+}
+
+*/
