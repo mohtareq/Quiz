cd3ee27f7df1be665ee1b9a9bd412393bd12d8ba
hamzaaaldirawi <hamza.dirawie@gmail.com>
2021-09-11 18:47:10 +0300

    Add Open Box JS

--- BoxesPage/boxes.js
+++ BoxesPage/boxes.js
@@ -3,3 +3,8 @@ function PopUp(){
     document.querySelectorAll('.scrollbar-hidden')[0].style.overflow = "auto"
 }
 
+for (let x = 0; x < 12; x++) {
+    $(`#box-${x}`).click(function() {
+        document.getElementById(`img-${x+1}`).src="../img/new/box-03.png";
+    })
+}
